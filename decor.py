ea67a885ac513178bd80180f7d7ac270a7635aff
Thierry Chappuis <tchappui@users.noreply.github.com>
2020-03-20 09:19:46 +0100

    Update decor.py

--- decor.py
+++ decor.py
@@ -1,3 +1,5 @@
+""" -tc- Ajouter une docstring."""
+
 class Decor:
 	'''
 	In charge of decor. Managing walls, hallways, enter and exit
@@ -7,17 +9,21 @@ class Decor:
 	HALLWAYS = ' ' #Define what is a hallway
 
 	def __init__(self, file):
+		""" -tc- Ajouter une docstring."""
 		self.walls, self.hallways = self._get_walls_and_hallways(file)
 		self.enter, self.exit = self._get_enter_and_exit()
 		
 	def _get_walls_and_hallways(self, file):
+		""" -tc- Ajouter une docstring."""
 		#Building two sets of walls and hallways based on a maze file
 		pass
 		
 	def _get_enter_and_exit(self):
+		""" -tc- Ajouter une docstring."""
 		#Getting enter and exit coordinates from the hallways set
 		pass
 	
 	def is_hallways(self, coordinates):
+		""" -tc- Ajouter une docstring."""
 		#Checking if coordinates are an hallway... or walls.
 		pass
